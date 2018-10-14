01bea4ae61fc4d7ca4c0808c6ab21edf45099fda
BadBoy <42121005+badboyzz@users.noreply.github.com>
2018-10-14 17:46:48 +0200

    Solved a bug

--- start.php
+++ start.php
@@ -1,9 +1,9 @@
 <?php
 
-$token  = "TOKEN"; //Botfather's api key
+$token  = "Token"; //Botfather's api key
 $admin  =  [430610430]; // ID of the admins
 $config = [
-    "db" => true, // true for using database
+    "db" => false, // true for using database
     "ip" => "localhost", 
     "user" => "root", 
     "password" => "password", 
@@ -11,13 +11,19 @@ $config = [
     "table" => "bot", 
     "debug_mode" => false //true for showing all errors and notice
 ];
+
+$save = array(
+    "save",
+    "token",
+    "config",
+    "db",
+    "disable",
+    "offset",
+    "admin" //list of variable
+);
+
 if ($config['db']) {
     $db = new PDO("mysql:host=" . $config["ip"] . ";dbname=" . $config['database'], $config['user'], $config['password']);
-}
-if ($config['debug_mode']) {
-    error_reporting(E_ALL);
-} else {
-    error_reporting(0);
 }
 
 echo "[INFO] Starting the bot...\n";
@@ -27,14 +33,15 @@ $up1    = json_decode($c1, true);
 $offset = $up1["result"][0]["update_id"];
 if (!$offset) {
     echo "\n[ERROR] Telegram returned an offset error!\n";
-    exit;
 }
 echo "[INFO] Starting fetching updates...\n";
 while (1) {
+    $l = file_get_contents("last.json");
     $content = file_get_contents("http://api.telegram.org/bot$token/getUpdates?offset=$offset");
-    if ($content == '{"ok":true,"result":[]}') {
+    if ($l == $content || $content == '{"ok":true,"result":[]}') {
     } else {
         $offset++;
+        file_put_contents("last.json", $content);
         $update = json_decode($content, true);
         $update = $update["result"][0];
         include("vars.php");
@@ -47,4 +54,22 @@ while (1) {
     foreach ($plugins as $plugin) {
             include("plugins/$plugin");
         }
+
+        $vars = array_keys(get_defined_vars());
+        foreach ($vars as $var) {
+            if (in_array($var, $save)) {
+            } else {
+                unset($$var);
+            }
+        }
+        unset($vars);
+}
+
+if ($config['debug_mode']) {
+    error_reporting(E_ALL);
+    if ($update){
+        echo "Got an update!";
+    }
+} else {
+    error_reporting(0);
 }
